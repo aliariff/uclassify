f324142215ada4d5768aabc259bc89a914bb403f
Ali Ariff <ali.ariff12@gmail.com>
2017-06-04 17:59:35 +0200

    simplify using GET

--- app.rb
+++ app.rb
@@ -8,23 +8,21 @@ get '/' do
   'Hello world!'
 end
 
-post '/language-detector' do
+get '/language-detector' do
   do_request('uclassify', 'language-detector')
 end
 
-post '/sport-topics' do
+get '/sport-topics' do
   do_request('uclassify', 'sport-topics')
 end
 
 def do_request(username, classifier_name)
   content_type :json
-  body = JSON.parse(request.body.read.to_s)
-  query = body['text']
-  query = [query] if query.class == String
+  query = params[:text]
   endpoint = URI("https://api.uclassify.com/v1/#{username}/#{classifier_name}/classify")
   req = Net::HTTP::Post.new(endpoint, 'Content-Type' => 'application/json')
   req.add_field('Authorization', "Token #{READ_KEY}")
-  req.body = { texts: query }.to_json
+  req.body = { texts: [query] }.to_json
   res = Net::HTTP.start(endpoint.host, endpoint.port, use_ssl: true) do |http|
     http.request(req)
   end
